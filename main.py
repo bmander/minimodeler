4262092e2b6a302b4e14f11ae244a4cd46e71e2e
Brandon Martin-Anderson <badhill@gmail.com>
2016-02-27 14:13:51 -0500

    add and move points in front view

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Tkinter import *
 
 class App:
     def __init__(self,master):
+        self.master = master
 
         self.front = Canvas(master, width=300, height=300, highlightbackground="black",highlightthickness=1)
         self.front.place(x=5,y=310)
@@ -12,24 +13,28 @@ class App:
         self.right = Canvas(master, width=300, height=300, highlightbackground="black",highlightthickness=1)
         self.right.place(x=310,y=310)
 
-        self.px,self.py=None,None
-
         self.front.bind("<B1-Motion>", self.on_move)
+
+        self.front.bind("<ButtonRelease-1>", self.on_buttonrelease)
+        self.front.bind("<Shift-Button-1>", self.on_shift_click)
         self.front.bind("<Button-1>", self.on_click)
+
+        self.selected_points = None
 
     def on_move(self, event):
 
-        if self.px is not None and self.py is not None:
-            self.front.create_line(self.px,self.py,event.x,event.y)
-            self.top.create_line(self.px,self.py,event.x,event.y)
-            self.right.create_line(self.px,self.py,event.x,event.y)
-
-        self.px = event.x
-        self.py = event.y
+        if self.selected_points is not None:
+            for point in self.selected_points:
+                self.front.coords(point, event.x-2.5,event.y-2.5,event.x+2.5,event.y+2.5)
 
     def on_click(self,event):
-        self.front.delete("all")
-        self.px,self.py=event.x,event.y
+        self.selected_points = self.front.find_overlapping(event.x-2,event.y-2,event.x+2,event.y+2)
+
+    def on_shift_click(self,event):
+        self.front.create_rectangle(event.x-2.5,event.y-2.5,event.x+2.5,event.y+2.5)
+
+    def on_buttonrelease(self,event):
+        self.selected_points = None
 
 master = Tk()
 master.resizable(width=False, height=False)
