976ad8b98c6956a9e6e8f26658ece1c97a4f6f78
Brandon Martin-Anderson <badhill@gmail.com>
2016-02-27 11:32:56 -0500

    rudamentary 3-view

--- main.py
+++ main.py
@@ -3,27 +3,37 @@ from Tkinter import *
 class App:
     def __init__(self,master):
 
-        self.w = Canvas(master, width=200, height=100)
-        self.w.pack()
+        self.front = Canvas(master, width=300, height=300, highlightbackground="black",highlightthickness=1)
+        self.front.place(x=5,y=310)
 
-        self.w.create_line(0, 0, 200, 100)
-        self.w.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
+        self.top = Canvas(master, width=300, height=300, highlightbackground="black",highlightthickness=1)
+        self.top.place(x=5,y=5)
 
-        self.w.create_rectangle(50, 25, 150, 75, fill="blue")
+        self.right = Canvas(master, width=300, height=300, highlightbackground="black",highlightthickness=1)
+        self.right.place(x=310,y=310)
 
         self.px,self.py=None,None
 
-        self.w.bind("<B1-Motion>", self.callback)
+        self.front.bind("<B1-Motion>", self.on_move)
+        self.front.bind("<Button-1>", self.on_click)
 
-    def callback(self, event):
+    def on_move(self, event):
+
         if self.px is not None and self.py is not None:
-            self.w.create_line(self.px,self.py,event.x,event.y)
+            self.front.create_line(self.px,self.py,event.x,event.y)
+            self.top.create_line(self.px,self.py,event.x,event.y)
+            self.right.create_line(self.px,self.py,event.x,event.y)
 
-        #print "clicked at", event.x, event.y
         self.px = event.x
         self.py = event.y
 
+    def on_click(self,event):
+        self.front.delete("all")
+        self.px,self.py=event.x,event.y
+
 master = Tk()
+master.resizable(width=False, height=False)
+master.geometry('{}x{}'.format(620, 620))
 app = App(master)
 
 mainloop()
